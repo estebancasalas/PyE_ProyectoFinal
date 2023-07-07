2bbf784bf234430b3f85e5ff7ae0a0ce3d671aef
SofiaPareja <sofiaparejac@gmail.com>
2023-07-07 16:37:03 -0300

    parte salario

---
+++ Salario.py
@@ -0,0 +1,110 @@
+import csv
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statistics
+import numpy as np
+
+
+def SalarioLista(filename):
+    salarios = [] # Lista para almacenar los salarios
+    with open(filename,'r') as file:
+        reader = csv.reader(file, delimiter=';')
+        reader.__next__() # Salta la primera linea, que es la de los nombres de las columnas
+        for row in reader:
+            salario_str = row[8]  # Obtén la cadena del salario de la columna 8 (índice 8)
+            salario_str = salario_str.replace(',', '.')  # Reemplaza la coma por un punto en la cadena
+            salario = float(salario_str)  # Convierte la cadena modificada a float
+   
+            salarios.append(salario)
+    return salarios
+
+
+def HistogramaSalario(filename):
+    
+    salarios = SalarioLista(filename)
+    particion = [0,25000,50000,75000,100000,125000,150000,175000,200000]
+    plt.hist(salarios,bins=particion, edgecolor='black')
+    plt.xlabel('Salario')
+    plt.ylabel('Frecuencia')
+    plt.title('Histograma de Salarios')
+    plt.show()
+
+def BoxplotSalario(filename):
+    salarios = [] # Lista para almacenar los salarios
+
+    with open(filename,'r') as file:
+        reader = csv.reader(file, delimiter=';')
+        reader.__next__() # Salta la primera linea, que es la de los nombres de las columnas
+        for row in reader:
+            salario_str = row[8]  # Obtén la cadena del salario de la columna 8 (índice 8)
+            salario_str = salario_str.replace(',', '.')  # Reemplaza la coma por un punto en la cadena
+            salario = float(salario_str)  # Convierte la cadena modificada a float
+   
+            salarios.append(salario)
+
+    particion = [0,25000,50000,75000,100000,125000,150000,175000,200000]
+    sns.boxplot(salarios)
+    plt.ylabel('Salarios')
+    plt.title('Histograma de Salarios')
+    plt.show()
+
+
+def medianaSalarios(filename):
+    salarios = SalarioLista(filename)
+
+    mediana = statistics.median(salarios)
+    return mediana
+
+def modaSalarios(filename):
+    salarios = SalarioLista(filename)
+
+    moda = statistics.mode(salarios)
+    return moda
+
+def mediaSalarios(filename):
+    salarios = SalarioLista(filename)
+
+    media = statistics.mean(salarios)
+    return media
+
+
+
+
+def quartiles(filename):
+    salarios = SalarioLista(filename)
+
+    cuartil_25 = np.percentile(salarios, 25)
+    cuartil_50 = np.percentile(salarios, 50)
+    cuartil_75 = np.percentile(salarios, 75)
+
+    return cuartil_25, cuartil_50, cuartil_75
+
+# Ejemplo de uso
+
+def maximoSalarios(filename):
+    salarios = SalarioLista(filename)
+
+    return max(salarios)
+
+def minimoSalarios(filename):
+    salarios = SalarioLista(filename)
+
+    return min(salarios)
+
+
+
+
+
+
+print(minimoSalarios('ECH_2022.csv'))
+print(maximoSalarios('ECH_2022.csv'))
+
+
+q1, q2, q3 = quartiles('ECH_2022.csv')
+print("Cuartil 25%:", q1)
+print("Cuartil 50%:", q2)
+print("Cuartil 75%:", q3)
+
+print(medianaSalarios('ECH_2022.csv'))
+print(modaSalarios('ECH_2022.csv'))
+print(mediaSalarios('ECH_2022.csv'))
